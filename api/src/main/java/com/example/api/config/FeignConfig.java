c1990ec0ec35f7ca7559040cb687b6ad8efb748d
Sky_Taill <vorobeichik2015@yandex.ru>
2023-06-27 00:59:12 +0300

    fix + add module

--- api/src/main/java/com/example/api/config/FeignConfig.java
+++ api/src/main/java/com/example/api/config/FeignConfig.java
@@ -1,18 +1,53 @@
 package com.example.api.config;
 
+
 import com.example.api.category.api.CategoryGateway;
 import com.example.api.manufacturer.api.ManufacturerGateway;
 import com.example.api.product.api.ProductGateway;
+import feign.*;
+import feign.codec.ErrorDecoder;
+import feign.optionals.OptionalDecoder;
+import feign.slf4j.Slf4jLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
-@EnableFeignClients(clients = {
-CategoryGateway.class,
- ManufacturerGateway.class,
-        ProductGateway.class
-})
+@EnableFeignClients
+@EnableConfigurationProperties(GbApiProperties.class)
+@RequiredArgsConstructor
+@Import(value = {FeignClientFactory.class})
 public class FeignConfig {
 
+    private final GbApiProperties gbApiProperties;
+    private final FeignClientFactory feignClientFactory;
+
+    @Bean
+    public CategoryGateway categoryGateway() {
+        return feignClientFactory.newFeignClient(CategoryGateway.class, gbApiProperties.getEndpoint().getCategoryUrl());
+    }
+
+    @Bean
+    public ManufacturerGateway manufacturerGateway() {
+        return feignClientFactory.newFeignClient(ManufacturerGateway.class, gbApiProperties.getEndpoint().getManufacturerUrl());
+    }
+
+    @Bean
+    public ProductGateway productGateway() {
+        return feignClientFactory.newFeignClient(ProductGateway.class, gbApiProperties.getEndpoint().getProductUrl());
+    }
 
 }
--- external-api/src/main/java/com/example/externalapi/ExternalApiApplication.java
+++ external-api/src/main/java/com/example/externalapi/ExternalApiApplication.java
@@ -2,8 +2,10 @@ package com.example.externalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+
 public class ExternalApiApplication {
 
     public static void main(String[] args) {
